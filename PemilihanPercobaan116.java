2f7ddc8d652fb3139e360d9aa40abc7d2edbe718
EksaMuhammad <144184459+EksaMuhammad@users.noreply.github.com>
2023-09-27 12:47:16 +0700

    Tugas jobsheet 5

--- PemilihanPercobaan116.java
+++ PemilihanPercobaan116.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 public class PemilihanPercobaan116 {
     public static void main(String[] args) {
         Scanner input16 = new Scanner(System.in);
+
+        String hasil;
+
         System.out.println("Masukkan angka: ");
         int angka = input16.nextInt();
 
-        if (angka % 2 == 0)
-        System.out.println("Angka " +angka+ " bilangan genap");
-        else
-        System.out.println("Angka " +angka+ " bilangan ganjil");
+        if (angka % 2 == 0) {
+             hasil="bilangan genap";
+        }
+        else {
+            hasil="bilangan ganjil";
+        }
 
+        System.out.println(angka+ " adalah " +hasil);
     }
 }
